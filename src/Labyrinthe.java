fb9f3479c219e2e355e25aa94e572d7b9a54e34d
Dandalex <dana16@uqo.ca>
2023-11-20 14:32:38 -0500

    draft 1

---
+++ src/Labyrinthe.java
@@ -0,0 +1,28 @@
+public class Labyrinthe{
+
+    /** 
+    À faire: cette fonction doit afficher le labyrinthe sur 20 lignes et 40 colonnes
+    */
+    public static void afficher(int[][] grid){
+      // le contenu de la fonction doit être remplacé... 
+      // les lignes ci-dessous indiquent comment utiliser les caractères "blocs"  
+        for (int i=0;i<10;i++){			
+            System.out.print("\u2588\u2588"); // utiliser ce caractère pour faire les murs
+            System.out.print("  ");
+        }
+        System.out.println();
+        System.out.println("Cette methode doit afficher le labyrinthe");
+        for (int i=0;i<10;i++){
+          System.out.print("\u2588\u2588");
+          System.out.print("  ");
+        }
+        System.out.println();
+
+    }
+
+    //resout le labyrinthe
+    public static boolean resoudre(int[][] grille){
+        return true;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // ce code n'a pas besoin d'être modifié
+        int[][] labyrinthe = charger("labyrinthe1.txt"); // ici on peut choisir le  labyrinthe parmi: "labyrinthe1.txt", "labyrinthe11.txt", "labyrinthe2.txt",  "labyrinthe21.txt", "labyrinthe3.txt", "labyrinthe31.txt".
+
+        // fonction pour afficher le labyrinthe (à faire)
+        Labyrinthe.afficher(labyrinthe);
+
+        // fonction pour résoudre le labyrinthe (à faire)
+        boolean solution = Labyrinthe.resoudre(labyrinthe);
+        
+        if (solution == true){
+            System.out.println("Chemin trouvé!");
+        } else {
+            System.out.println("Chemin non trouvé!");
+        }
+        Labyrinthe.afficher(labyrinthe);
+
+    }
+
+
+
+    /* méthode fournie pour lire une grille dans un fichier*/ 
+    public static int[][] charger(String fichier) throws FileNotFoundException {		
+    File file = new File(fichier);
+    int[][] tableau = new int[20][20]; //grille 20x20
+
+      Scanner lectureFichier = new Scanner(file);
+      int count = 0;
+      for (int i = 0; i<20; i++) { // le fichier a 20 lignes
+          String ligne  = lectureFichier.nextLine();
+          char[] caracteres = ligne.toCharArray();
+          for (int c=0;c<caracteres.length;c++) {
+            tableau[i][c]= (caracteres[c]=='0'?0:1); // expression conditionnelle: si '0' alors 0 else 1
+        }
+      }
+      lectureFichier.close();
+
+      return tableau;
+
+  }
+
+}
