d9390d1971e20b196f3bf8b9aa8dd93010504566
Dandalex <dana16@uqo.ca>
2023-11-20 14:46:09 -0500

    can print labarynth

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -6,9 +6,17 @@ public class Labyrinthe{
     public static void afficher(int[][] grid){
       // le contenu de la fonction doit être remplacé... 
       // les lignes ci-dessous indiquent comment utiliser les caractères "blocs"  
-        for (int i=0;i<10;i++){			
-            System.out.print("\u2588\u2588"); // utiliser ce caractère pour faire les murs
-            System.out.print("  ");
+        for (int i=0;i<grid.length;i++){
+          for (int t=0;t<grid.length;t++){
+            if (grid[i][t]==1){			
+              System.out.print("\u2588\u2588"); // utiliser ce caractère pour faire les murs
+            }
+            else{
+              System.out.print("  ");
+            }  
+
+          }
+          System.out.println();
         }
         System.out.println();
         System.out.println("Cette methode doit afficher le labyrinthe");
