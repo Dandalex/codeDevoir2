3aca545c98f1bfa19a895326b3f946dae675bfe2
Alex D'Andrea <dana16@uqo.ca>
2023-11-21 15:06:17 -0500

    remove localication

--- src/Test.java
+++ src/Test.java
@@ -4,14 +4,13 @@ public class Test {
     
 
     public static void main(String[] args){
-        Localisation a= new Localisation(0, 0);
+ 
         /*Localisation b = new Localisation(0, 0);
 
         b.printLocal();*/
 
         int [][]grid = {{0,0,0,0,1},
                         {1,0,1,0,0}};
-        System.out.println(a.deplacementBasPossible(grid));
 
 
 
