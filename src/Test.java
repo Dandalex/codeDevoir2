ff44866935512bc38f13419d43fec6ec7ba822fe
Alex D'Andrea <dana16@uqo.ca>
2023-11-23 13:43:34 -0500

    working on testing phase

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
     
 
     public static boolean resoudre (int grid[][]){
-        int y=0, x=1, a=1, b=0;
+        int y=0, x=1, ay=1, ax=0;
         boolean [][] reponse = new boolean[grid.length][grid[0].length];
         reponse[y][x]=true;
             for (y=0;y<20;y++){
@@ -19,4 +19,46 @@ public class Test {
 
         return false;
     }
+    
+    public static boolean verifierIntersection (int grid[][],int y , int x) {
+    	if (grid[y][x-1]==0 && grid[y+1][x]==0 && grid[y][x+1]==0||
+    		grid[y][x+1]==0 && grid[y+1][x]==0 && grid[y][x+1]==0|| 
+    		grid[y][x-1]==0 && grid[y-1][x]==0 && grid[y][x+1]==0||
+    		grid[y][y+1]==0 && grid[y-1][x]==0 && grid[y][x+1]==0){
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
+    
+    
+ public static char verifierOrigine ( int y , int ancienY, int x, int ancienX) {
+	 if(ancienX==x&&ay==y-1) {
+		 return 'h';
+	 }
+	 if(ax==x&&ay==y+1) {
+		 return 'b';
+	 }
+	 if(ax==x&&ay==y) {
+		 return 'b';
+	 }
+	 if(x==ax&&ay==y+1) {
+		 return 'b';
+	 }
+ }
+	 
+    public static boolean culDeSac (int grid[][],boolean[][] reponse,int y , int x) {
+    
+    	
+    	
+    	
+    	return false;
+    }
+    
+    
+    
+    
+    
+    
 }
